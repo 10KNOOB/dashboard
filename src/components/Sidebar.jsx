73068bbb22a6a1aeb1642bcc6e8e1cd860a7e686
Rashid Shahriar <rashidslink@gmail.com>
2023-02-22 13:37:04 +0600

    END OF THE VIDEO

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider.js";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu, screenSize, setScreenSize } =
+  const { activeMenu, setActiveMenu, screenSize, setScreenSize, currentColor } =
     useStateContext();
 
   const handleCloseSideBar = () => {
@@ -61,6 +61,9 @@ const Sidebar = () => {
                     to={`/${link.name}`}
                     key={link.name}
                     onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : "",
+                    })}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
